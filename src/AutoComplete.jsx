ced302682f876d505f81eea224b00270e43bfd15
SebastianCieslik <95974001+SebastianCieslik@users.noreply.github.com>
2022-11-24 18:43:24 +0100

    Created 2 inputs with definied values

--- src/AutoComplete.jsx
+++ src/AutoComplete.jsx
@@ -1,12 +1,27 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 export default function AutoComplete() {
 
-    const value = useState("Poland");
+    const [allValues, setAllValues] = useState({
+        search1: 'Poland',
+        search2: 'Monako'
+    });
+
+    const onChange = (event) => {
+      setValue(event.target.value);
+    };
+
+
   return (
     <>
-    <div>AutoComplete</div>
-    <p>{value}</p>
+    <form>
+  <label>
+    AutoComplete Search
+    <input type="text" name="search1" value={allValues.search1} />
+    <input type="text" name="search2" value={allValues.search2} />
+  </label>
+  <input type="submit" value="Submit" />
+</form>
     </>
   )
 }
