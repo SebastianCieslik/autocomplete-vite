2363dcac21d6a1586bee7cb1ff3dbd1499aa9fcb
SebastianCieslik <95974001+SebastianCieslik@users.noreply.github.com>
2022-11-24 16:45:01 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    Vite xD 
+  )
+}
+
+export default App
